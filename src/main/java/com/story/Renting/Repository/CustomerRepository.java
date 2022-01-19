d00df03dc944adc1a2e363ee13a58771c68f95e9
Aakash146 <aakash.123singh@yahoo.com>
2022-01-20 02:26:10 +0530

    change

--- src/main/java/com/story/Renting/Repository/CustomerRepository.java
+++ src/main/java/com/story/Renting/Repository/CustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findByEmail(String Email);
 
-    Customer findByFarmId(Long custId);
+    Customer findByCustId(Long custId);
 }
